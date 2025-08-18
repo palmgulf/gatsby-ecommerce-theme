1967e5c11ddd27b8d0ca06445ec81947d524a8e1
palmgulf <admin@palmgulf.com>
2025-08-17 22:53:51 -0700

    added gatsby-ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,10 @@
+// gatsby-ssr.js
+import React from 'react';
+import { NotificationProvider } from './src/context/AddItemNotificationProvider';
+import { CartProvider } from './src/context/cartContext';
+
+export const wrapRootElement = ({ element }) => (
+  <NotificationProvider>
+    <CartProvider>{element}</CartProvider>
+  </NotificationProvider>
+);
